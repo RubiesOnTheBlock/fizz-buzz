d234156d72bf88440f5403559c9bb640e7f03a72
raluka <badoi.raluca@gmail.com>
2015-05-11 19:38:07 +0200

    FizzBuzz to the max! Done

--- game.rb
+++ game.rb
@@ -1,17 +1,25 @@
 # The Fizz Buzz Game
 # Outputs numbers starting from 1 to 50
-# If the number is divisable by 3 output
+# If the number is divisible by 3 output
 # 'Fizz'. If the number is divisible by
 # 5 output 'Buzz'. If the number is
 # divisible by 3 and 5 output 'Fizz Buzz'.
 # In any other case output the number.
 
+
 number = 1
 
 while number <= 50 do
-   puts number
-   number = number + 1
-   if number % 5 == 0
+   if number % 15 == 0
+     puts 'FizzBuzz'
+   elsif number % 3 == 0
      puts 'Fizz'
+   elsif number % 5 == 0
+   else
+     puts number
    end
+  number += 1
 end
+
+
+
